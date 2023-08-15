a2c5660479c8eabd10853623c9570b3268c2a1fe
olgademirtash <olga.demirtash@gmail.com>
2023-08-15 08:30:56 +0300

    HW5 - Исправления замечаний от 11.08.2023

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
  */
 public abstract class AbstractArrayStorage extends AbstractStorage {
     protected static final int STORAGE_LIMIT = 10000;
-    protected Resume[] storage;
+    protected final Resume[] storage;
     protected int size = 0;
 
     {
